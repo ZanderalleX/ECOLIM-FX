f6b24aefd5fdfd2e25459831868a595a6c7f6d6d
ZanderalleX <alexanderjosuegradoscastro@gmail.com>
2026-06-10 14:21:56 -0500

    Primer commit

---
+++ HelloWorld/src/vista/MenuPrincipal.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+import dao.UsuarioDAO;
+import modelo.Usuario;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author LAB-USR-HUANCAYO
+ */
+public class MenuPrincipal extends Stage {
+
+    public MenuPrincipal(Usuario usuario) {
+        setTitle("ECOLIM");
+      
+    }
+}
